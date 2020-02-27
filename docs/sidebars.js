48296cfbc71ca03816d0dfc592dec21757a74a56
Lukas Gentele <lg@devspace.cloud>
2020-02-26 22:10:10 -0800

    docs: minor improvements

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -85,7 +85,7 @@ module.exports = {
     {
       type: 'link',
       label: '↗️ DevSpace CLI',
-      href: 'https://devspace.sh/docs/introduction',
+      href: 'https://devspace.sh/cli/docs/introduction',
     },
   ],
 };
